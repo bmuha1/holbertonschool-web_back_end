ef95152b3c5d2c121400150d4f5d85578692fc23
Brent Muha <brent.muha@gmail.com>
2020-10-19 14:12:47 -0700

    Remove mypy errors

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -9,13 +9,13 @@ def zoom_array(lst: Tuple, factor: int = 2) -> List:
     ''' Type checking '''
     zoomed_in: List = [
         item for item in lst
-        for i in range(int(factor))
+        for i in range(factor)
     ]
     return zoomed_in
 
 
 array = [12, 72, 91]
 
-zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(tuple(array))
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(tuple(array), int(3.0))
