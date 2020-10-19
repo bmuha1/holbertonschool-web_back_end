6074f2c6ba0d28dd1ee6e971b53858703a65dc21
Brent Muha <brent.muha@gmail.com>
2020-10-19 10:20:59 -0700

    Add basic annotations add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+Take two floats as arguments and return their sum as a float
+'''
+
+
+def add(a: float, b: float) -> float:
+    ''' Take two floats as arguments and return their sum as a float '''
+    return a + b
