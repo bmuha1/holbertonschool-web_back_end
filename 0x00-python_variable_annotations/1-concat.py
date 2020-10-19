42163bf237a73f0270343ea615dba14f412bb3d5
Brent Muha <brent.muha@gmail.com>
2020-10-19 10:26:54 -0700

    Add basic annotations concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''
+Take two strings as arguments and return a concatenated string
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' Take two strings as arguments and return a concatenated string '''
+    return str1 + str2
