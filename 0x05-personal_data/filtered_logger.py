1be948fd2fbe9d763ad38a3ad87f0f5f17b946f6
Brent Muha <brent.muha@gmail.com>
2020-10-30 15:38:31 -0700

    Add filter datum

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Return the log message obfuscated
+"""
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """Return the log message obfuscated
+    """
+    for f in fields:
+        message = re.sub(f'(?<={f}=)[^{separator}]*', redaction, message)
+    return message
