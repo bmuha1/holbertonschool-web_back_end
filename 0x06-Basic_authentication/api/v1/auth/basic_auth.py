8457a69f8c408d25a597276800634c7216a0ce64
Brent Muha <brent.muha@gmail.com>
2020-11-04 15:58:33 -0800

    Add basic auth

---
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+BasicAuth class to manage API authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """BasicAuth class to manage API authentication
+    """
