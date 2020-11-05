5b932345a49efe7163f58158c524bb9bce0f82ff
Brent Muha <brent.muha@gmail.com>
2020-11-05 14:34:18 -0800

    Add base64 decode

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 BasicAuth class to manage API authentication
 """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -16,3 +17,13 @@ class BasicAuth(Auth):
         if (isinstance(authorization_header, str) and
                 authorization_header.startswith('Basic ')):
             return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+            self, base64_authorization_header: str) -> str:
+        """Return the decoded value of a Base64 string
+        """
+        try:
+            return base64.b64decode(
+                base64_authorization_header.encode('utf-8')).decode('utf-8')
+        except:
+            return
