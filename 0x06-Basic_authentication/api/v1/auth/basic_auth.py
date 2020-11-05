2bbad987a150b6e87fc0537c6967280a8b234e6c
Brent Muha <brent.muha@gmail.com>
2020-11-05 14:19:22 -0800

    Add authorization header

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,11 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """BasicAuth class to manage API authentication
     """
+
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """Return the Base64 part of the Authorization header
+        """
+        if (isinstance(authorization_header, str) and
+                authorization_header.startswith('Basic ')):
+            return authorization_header[6:]
