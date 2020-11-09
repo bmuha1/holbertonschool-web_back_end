c1c43052ea4b08ace48364d2451aa1f313d39ce5
Brent Muha <brent.muha@gmail.com>
2020-11-09 14:48:57 -0800

    Add empty SessionAuth class

---
+++ 0x07-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+SessionAuth class to manage API authentication
+"""
+from api.v1.auth.auth import Auth
+
+
+class SessionAuth(Auth):
+    """SessionAuth class to manage API authentication
+    """
