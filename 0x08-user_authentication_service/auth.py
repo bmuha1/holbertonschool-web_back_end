298218ef4a9af2f6afacf64b9eed518fa7a0a732
Brent Muha <brent.muha@gmail.com>
2020-11-16 14:41:09 -0800

    Add get session id

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -54,3 +54,15 @@ class Auth:
                                   user.hashed_password)
         except Exception:
             return False
+
+    def create_session(self, email: str) -> str:
+        """
+        Create new session
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except Exception:
+            return
