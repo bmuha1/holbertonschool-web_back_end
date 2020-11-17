b308442bb16669f25bca4193a9ee8c4a6cf19302
Brent Muha <brent.muha@gmail.com>
2020-11-17 10:44:30 -0800

    Add destroy session

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -75,3 +75,12 @@ class Auth:
             return self._db.find_user_by(session_id=session_id)
         except Exception:
             return
+
+    def destroy_session(self, user_id: int) -> None:
+        """
+        Update the user's session ID to None
+        """
+        try:
+            self._db.update_user(user_id, session_id=None)
+        except Exception:
+            return
