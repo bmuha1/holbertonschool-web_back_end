54b4457864370dcce1caecad0005a6c6ef803319
Brent Muha <brent.muha@gmail.com>
2020-11-17 14:15:50 -0800

    Add update password

--- 0x08-user_authentication_service/auth.py
+++ 0x08-user_authentication_service/auth.py
@@ -102,3 +102,14 @@ class Auth:
             return reset_token
         except Exception:
             raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """
+        Update user password
+        """
+        user = self._db.find_user_by(reset_token=reset_token)
+        if not user:
+            raise ValueError
+        self._db.update_user(user.id,
+                             hashed_password=_hash_password(password),
+                             reset_token=None)
