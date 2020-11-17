0c34c5c405c56bd14c1cc1c347f90e70f794ad56
Brent Muha <brent.muha@gmail.com>
2020-11-17 11:21:07 -0800

    Import redirect

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 SQLAlchemy model User
 """
-from flask import Flask, jsonify, request, abort
+from flask import Flask, jsonify, request, abort, redirect
 from auth import Auth
 
 app = Flask(__name__)
