470a0d5ed530a44f4b031fb9abe1df19210214a7
Brent Muha <brent.muha@gmail.com>
2020-11-17 10:51:25 -0800

    Abort if user doesn't exist

--- 0x08-user_authentication_service/app.py
+++ 0x08-user_authentication_service/app.py
@@ -69,10 +69,10 @@ def logout():
     """
     session_id = request.cookies.get('session_id')
     user = AUTH.get_user_from_session_id(session_id)
-    try:
+    if user:
         AUTH.destroy_session(user.id)
         return redirect('/')
-    except Exception:
+    else:
         abort(403)
 
 
