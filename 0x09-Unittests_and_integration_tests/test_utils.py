e887c9cfebb75d3046716b628af13998a2094df0
Brent Muha <brent.muha@gmail.com>
2020-11-20 09:58:00 -0800

    Add parameterize a unit test

---
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Unittests for utils
+"""
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """
+    Unittests for nested map
+    """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {'b': 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self, nested_map, path, map):
+        """
+        Test access nested map
+        """
+        self.assertEqual(access_nested_map(nested_map, path), map)
+
+
+if __name__ == '__main__':
+    unittest.main()
