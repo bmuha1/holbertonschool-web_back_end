dcf099bfc8acf3f2f58eadb4d9c86de6d8f73ba5
Brent Muha <brent.muha@gmail.com>
2020-11-24 10:29:37 -0800

    Add parameterized

--- 0x09-Unittests_and_integration_tests/test_client.py
+++ 0x09-Unittests_and_integration_tests/test_client.py
@@ -55,6 +55,17 @@ class TestGithubOrgClient(unittest.TestCase):
             my_mock.assert_called_once()
             public.assert_called_once()
 
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(self, repo, license_key, has_license):
+        """
+        Test has_license
+        """
+        g = client.GithubOrgClient('test')
+        self.assertEqual(g.has_license(repo, license_key), has_license)
+
 
 if __name__ == '__main__':
     unittest.main()
