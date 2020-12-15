557309d0fa73fc90a177270268723b7a7fc1f8c9
Brent Muha <800@holbertonschool.com>
2020-12-15 20:30:51 +0000

    Separate promises

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,7 +1,11 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
-    console.log(values[0].body, values[1].firstName, values[1].lastName);
-  }, () => console.log('Signup system offline'));
+  return uploadPhoto().then((photo) => {
+    createUser().then((user) => {
+      console.log(photo.body, user.firstName, user.lastName);
+    })
+      .catch(() => console.log('Signup system offline'));
+  })
+    .catch(() => console.log('Signup system offline'));
 }
