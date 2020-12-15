61e75b857b792ad52c989388977c9d79982bbffc
Brent Muha <800@holbertonschool.com>
2020-12-15 20:06:23 +0000

    Fix capitalization error

--- 0x0F-ES6_promise/2-then.js
+++ 0x0F-ES6_promise/2-then.js
@@ -1,7 +1,7 @@
 export default function handleResponseFromAPI(promise) {
   return promise.then(() => ({
     status: 200,
-    body: 'Success',
+    body: 'success',
   }))
     .catch(() => Error())
     .then(() => console.log('Got a response from the API'));
